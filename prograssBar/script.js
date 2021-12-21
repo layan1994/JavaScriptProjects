57e57f199c516c0047be1a3e256580d3697d18b8
layan1994 <layan1994>
2021-12-21 12:53:53 +0530

    second commit

---
+++ prograssBar/script.js
@@ -0,0 +1,41 @@
+
+const prograss = document.getElementById('prograss');
+const next = document.getElementById('next');
+const prev = document.getElementById('prev');
+const circles = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+
+next.addEventListener('click',() =>{
+    currentActive++;
+    if(currentActive > circles.length){
+        currentActive = circles.length;
+    }
+    update();
+});
+prev.addEventListener('click',() =>{
+    currentActive--;
+
+    if(currentActive < 1){
+        currentActive = 1;
+    }
+    update();
+});
+
+function update(){
+    circles.forEach((circle , idx ) => {
+        if (idx < currentActive){
+            circle.classList.add( 'active' );
+        }else{
+            circle.classList.remove( 'active');
+        }
+    });
+    const actives = document.querySelectorAll( '.active' );
+    prograss.style.width = ((actives.length - 1 )/(circles.length - 1)) * 100 +'%';
+
+}
+
+ 
+//if (currentActive === 1){
+//    prev.disabled = true;
+//}
